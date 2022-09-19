e028cd540cbf8debfa0b116037569fcb04a0d8f1
jerry <calljerryli@outlook.com>
2022-09-19 17:17:11 +0800

    获取摄像头和麦克风

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const getMediaDevices = () => {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    })
+      .then(stream => {
+        console.log('stream', stream)
+      })
+  }
   return (
     <div>
+      <button onClick={getMediaDevices}>获取摄像头和麦克风</button>
       <video></video>
     </div>
   )

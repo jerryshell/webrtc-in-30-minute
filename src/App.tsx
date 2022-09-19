de2f27fb97c7ec663da42ec45541df5c40b7bd2f
jerry <calljerryli@outlook.com>
2022-09-19 18:54:45 +0800

    创建 Offer

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,17 @@ function App() {
     console.log('rtc 连接创建成功', _pc)
   }
 
+  const createOffer = () => {
+    pc.current?.createOffer({
+      offerToReceiveVideo: true,
+      offerToReceiveAudio: true,
+    })
+      .then(sdp => {
+        console.log('offer', JSON.stringify(sdp))
+        pc.current?.setLocalDescription(sdp)
+      })
+  }
+
   return (
     <div>
       <button onClick={getMediaDevices}>获取摄像头和麦克风</button>
@@ -35,6 +46,8 @@ function App() {
       <video style={{ width: '400px' }} ref={localVideoRef} autoPlay></video>
       <br />
       <button onClick={createRtcConnection}>创建 RTC 连接</button>
+      <br />
+      <button onClick={createOffer}>创建 Offer</button>
     </div>
   )
 }

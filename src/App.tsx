f13ccff6c8c02a84c9efc99bd42c5f370b00b8a1
jerry <calljerryli@outlook.com>
2022-09-19 18:40:37 +0800

    创建 RTC 连接

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 
 function App() {
   const localVideoRef = useRef<HTMLVideoElement>(null)
+  const pc = useRef<RTCPeerConnection>()
 
   const getMediaDevices = () => {
     navigator.mediaDevices.getUserMedia({
@@ -15,11 +16,25 @@ function App() {
       })
   }
 
+  const createRtcConnection = () => {
+    const _pc = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: ['stun:stun.stunprotocol.org:3478'],
+        }
+      ]
+    })
+    pc.current = _pc
+    console.log('rtc 连接创建成功', _pc)
+  }
+
   return (
     <div>
       <button onClick={getMediaDevices}>获取摄像头和麦克风</button>
       <br />
       <video style={{ width: '400px' }} ref={localVideoRef} autoPlay></video>
+      <br />
+      <button onClick={createRtcConnection}>创建 RTC 连接</button>
     </div>
   )
 }

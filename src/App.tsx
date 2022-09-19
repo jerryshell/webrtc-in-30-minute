6c36e5af828c91125bf0f29556d2ca4e248277a9
jerry <calljerryli@outlook.com>
2022-09-19 17:39:37 +0800

    在页面显示摄像头图像

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+  const localVideoRef = useRef<HTMLVideoElement>(null)
+
   const getMediaDevices = () => {
     navigator.mediaDevices.getUserMedia({
       video: true,
-      audio: true,
+      audio: false,
     })
       .then(stream => {
         console.log('stream', stream)
+        localVideoRef.current!.srcObject = stream
       })
   }
+
   return (
     <div>
       <button onClick={getMediaDevices}>获取摄像头和麦克风</button>
-      <video></video>
+      <br />
+      <video style={{ width: '400px' }} ref={localVideoRef} autoPlay></video>
     </div>
   )
 }
